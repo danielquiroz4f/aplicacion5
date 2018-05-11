d814ca4b637e8e7d2c697a34ffeb1b34969e833a
ligartijax3 <38224756+ligartijax3@users.noreply.github.com>
2018-05-11 11:08:41 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -3,13 +3,11 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		$('#sonar').tap(function(){
 			navigator.notification.beep(2);
-		}); //sonar
-		$('#vibrar').tap(function() {
+		}); //tap sonar
+		$('#vibrar').tap(function(){
 			navigator.notification.vibrate(2000);
-		}); //vibrar
-	},false); // cierre device ready
+		}); //tap vibrar
+	},false); // cierre deviceready
 }); //cierre del ready
 	
-			
-			
-			
+			 	 
